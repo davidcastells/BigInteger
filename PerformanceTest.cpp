2d5c889899544618be957c57497495e125e47ed3
David Castells-Rufas <david.castells@uab.es>
2017-05-16 16:26:26 +0200

    verbosity

--- PerformanceTest.cpp
+++ PerformanceTest.cpp
@@ -1,8 +1,18 @@
-/* 
- * File:   PerformanceTest.cpp
- * Author: dcr
- * 
- * Created on April 24, 2017, 4:16 PM
+/*
+ * Copyright (C) 2017 Universitat Autonoma de Barcelona - David Castells-Rufas <david.castells@uab.cat>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "PerformanceTest.h"
@@ -29,14 +39,31 @@ PerformanceTest::~PerformanceTest()
  */
 void PerformanceTest::run()
 {
-   cout << "[Performance TESTS]" << endl;
+    cout << "[Performance TESTS]" << endl;
     
-   testPerformanceRange();
-   testPerformanceAdd(); 
-   testPerformanceSubtract(); 
-   testPerformanceMult();
-   testPerformanceModMult();
-   testPerformanceModPow();
+    BigInteger::extraChecks = 1;
+
+    testPerformanceFinalModPow();
+   
+    cout << "[Complete]" << endl;
+
+}
+
+void PerformanceTest::runAll()
+{
+    cout << "[Performance TESTS]" << endl;
+    
+    BigInteger::extraChecks = 1;
+
+    testPerformanceRange();
+    testPerformanceAdd(); 
+    testPerformanceSubtract(); 
+    testPerformanceMult();
+    testPerformanceModMult();
+    testPerformanceModPow();
+   
+    cout << "[Complete]" << endl;
+
 }
 
 void PerformanceTest::testPerformanceRange()
@@ -149,9 +176,42 @@ void PerformanceTest::testPerformanceMult()
         BigInteger r;
         a.initSize(bits/32);
         b.initSize(bits/32);
-        r.initSize(bits/32);
+        r.initSize((2*bits)/32);
         a.random();
         b.random();
+        
+        if (verbosity > 1)
+        {
+            cout << "a: " << a.toHexString() << endl;
+            cout << "b: " << b.toHexString() << endl;
+        }
+        
+        lap.start();
+        for (int rep=0; rep < numReps; rep++)
+            BigInteger::mult_naive(&r, &a, &b);
+        seconds = lap.stop();
+        
+        cout << "Multiply Naive;\t" << bits << ";\t" << seconds << ";" <<endl;
+    }
+    
+    for (int bits = 32; bits <= 4096; bits *= 2)
+    {
+        PerformanceLap lap;
+        
+        BigInteger a;
+        BigInteger b;
+        BigInteger r;
+        a.initSize(bits/32);
+        b.initSize(bits/32);
+        r.initSize((2*bits)/32);
+        a.random();
+        b.random();
+        
+        if (verbosity > 1)
+        {
+            cout << "a: " << a.toHexString() << endl;
+            cout << "b: " << b.toHexString() << endl;
+        }
         
         lap.start();
         for (int rep=0; rep < numReps; rep++)
@@ -170,9 +230,15 @@ void PerformanceTest::testPerformanceMult()
         BigInteger r;
         a.initSize(bits/32);
         b.initSize(bits/32);
-        r.initSize(bits/32);
+        r.initSize((2*bits)/32);
         a.random();
         b.random();
+        
+        if (verbosity > 1)
+        {
+            cout << "a: " << a.toHexString() << endl;
+            cout << "b: " << b.toHexString() << endl;
+        }
         
         lap.start();
         for (int rep=0; rep < numReps; rep++)
@@ -191,9 +257,15 @@ void PerformanceTest::testPerformanceMult()
         BigInteger r;
         a.initSize(bits/32);
         b.initSize(bits/32);
-        r.initSize(bits/32);
+        r.initSize((2*bits)/32);
         a.random();
         b.random();
+        
+        if (verbosity > 1)
+        {
+            cout << "a: " << a.toHexString() << endl;
+            cout << "b: " << b.toHexString() << endl;
+        }
         
         lap.start();
         for (int rep=0; rep < numReps; rep++)
@@ -208,12 +280,13 @@ void PerformanceTest::testPerformanceModMult()
 {
      
     double seconds;
-    int numReps = 1000;
+    int numReps = 100;
+    int maxBits = 512;
     cout << "MODULAR MULTIPLY)" << endl;
     cout << "Modular Multiplying two random numbers ("<< (numReps/1000)<<"k times)" << endl;
     cout << "Details;\t\tbits;\ttime;" << endl;
     
-    for (int bits = 32; bits <= 4096; bits *= 2)
+    for (int bits = 32; bits <= maxBits; bits *= 2)
     {
         PerformanceLap lap;
         
@@ -311,9 +384,9 @@ void PerformanceTest::testPerformanceModMult()
 
 void PerformanceTest::testPerformanceModPow()
 {
-     
     double seconds;
     int numReps = 10;
+    int maxBits = 512;
     cout << "MODULAR EXPONENTIATION)" << endl;
     cout << "Modular Exponentiation two random numbers ("<< (numReps)<<" times)" << endl;
     cout << "Details;\t\tbits;\ttime;" << endl;
@@ -324,35 +397,9 @@ void PerformanceTest::testPerformanceModPow()
     BigInteger r, r2;
  
     
-    for (int bits = 32; bits <= 4096; bits *= 2)
-    {
-        PerformanceLap lap;
-     
-        a.initSize(bits/32);
-        e.initSize(bits/32);
-        m.initSize(bits/32);
-        r.initSize(bits/32);
-        r2.initSize(bits/32);
-        a.random();
-        e.random();
-        
-        BigInteger radix;
-        radix.initSize(m.m_size+1);
-        BigInteger mprime;
-        mprime.initSize(radix.m_size);        
-
-        m.random();
-        
-        lap.start();
-        for (int rep=0; rep < numReps; rep++)
-            BigInteger::powerMod(&r, &a, &e, &m);
-        seconds = lap.stop();
-        
-        cout << "Standard Modular Exponentiation;\t" << bits << ";\t" << seconds << ";" <<endl;
-
-    }
     
-    for (int bits = 32; bits <= 4096; bits *= 2)
+    
+    for (int bits = 32; bits <= maxBits; bits *= 2)
     {
         PerformanceLap lap;
      
@@ -389,4 +436,89 @@ void PerformanceTest::testPerformanceModPow()
         
         cout << "Montgomery Modular Exponentiation;\t" << bits << ";\t" << seconds << ";" <<endl;
     }
+    
+    for (int bits = 32; bits <= maxBits; bits *= 2)
+    {
+        PerformanceLap lap;
+     
+        a.initSize(bits/32);
+        e.initSize(bits/32);
+        m.initSize(bits/32);
+        r.initSize(bits/32);
+        r2.initSize(bits/32);
+        a.random();
+        e.random();
+        
+        BigInteger radix;
+        radix.initSize(m.m_size+1);
+        BigInteger mprime;
+        mprime.initSize(radix.m_size);        
+
+        m.random();
+        
+        lap.start();
+        for (int rep=0; rep < numReps; rep++)
+            BigInteger::powerMod(&r, &a, &e, &m);
+        seconds = lap.stop();
+        
+        cout << "Standard Modular Exponentiation;\t" << bits << ";\t" << seconds << ";" <<endl;
+
+    }
+}
+
+void PerformanceTest::testPerformanceFinalModPow()
+{
+    double seconds;
+    int numReps = 100;
+    int maxBits = 2048;
+    cout << "MODULAR EXPONENTIATION)" << endl;
+    cout << "Modular Exponentiation two random numbers ("<< (numReps)<<" times)" << endl;
+    cout << "Details;\t\tbits;\ttime;" << endl;
+    
+    BigInteger a;
+    BigInteger e;
+    BigInteger m;
+    BigInteger r, r2;
+ 
+    
+    
+    
+    for (int bits = 32; bits <= maxBits; bits *= 2)
+    {
+        PerformanceLap lap;
+     
+        a.initSize(bits/32);
+        e.initSize(bits/32);
+        m.initSize(bits/32);
+        r.initSize(bits/32);
+        r2.initSize(bits/32);
+        a.random();
+        e.random();
+        
+        BigInteger radix;
+        radix.initSize(m.m_size+1);
+        BigInteger mprime;
+        mprime.initSize(radix.m_size);        
+
+        do
+        {
+
+            m.random();
+//        cout << "m: " << m.toHexString() << endl;
+
+            BigInteger::radixFromMontgomeryMod(&radix, &m);
+        
+            BigInteger::mprimeFromMontgomeryRadix(&mprime, &m, &radix);
+        
+        } while (mprime.isZero());
+        
+        
+        lap.start();
+        for (int rep=0; rep < numReps; rep++)
+            BigInteger::powerModMontgomery(&r2, &a, &e, &m, &mprime, &radix);
+        seconds = lap.stop();
+        
+        cout << "Montgomery Modular Exponentiation;\t" << bits << ";\t" << seconds << ";" <<endl;
+    }
+    
 }
--- PerformanceTest.h
+++ PerformanceTest.h
@@ -1,8 +1,18 @@
-/* 
- * File:   PerformanceTest.h
- * Author: dcr
+/*
+ * Copyright (C) 2017 Universitat Autonoma de Barcelona - David Castells-Rufas <david.castells@uab.cat>
  *
- * Created on April 24, 2017, 4:16 PM
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef PERFORMANCETEST_H
@@ -16,6 +26,7 @@ public:
 
 public:
     void run();
+    void runAll();
     
 private:
     void testPerformanceRange();
@@ -24,7 +35,11 @@ private:
     void testPerformanceMult();
     void testPerformanceModMult();
     void testPerformanceModPow();
+    void testPerformanceFinalModPow();
 
+
+public:
+    int verbosity;
 };
 
 #endif	/* PERFORMANCETEST_H */
