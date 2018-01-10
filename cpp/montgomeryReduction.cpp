0121dafba72ddefa151d4f916f06da35d10dff31
David Castells-Rufas <david.castells@uab.es>
2018-01-10 16:47:37 +0100

    non static method

--- cpp/montgomeryReduction.cpp
+++ cpp/montgomeryReduction.cpp
@@ -19,6 +19,18 @@
 #include <iostream>
 
 /**
+ * Computes this = this * R^-1 mod m
+ * @param m
+ * @param radix
+ * @param mprime
+ */
+void BigInteger::montgomeryReduction(BigInteger* m, BigInteger* radix, unsigned int mprime)
+{
+    BigInteger ref(*this);
+    BigInteger::montgomeryReduction(this, &ref, m, radix, mprime);
+}
+
+/**
  * Algorithm 14.32 from Handbook from applied cryptography
  * Compute montgomery reduction
  * base b= 2^32
@@ -88,3 +100,31 @@ void BigInteger::montgomeryReduction(BigInteger* r, BigInteger* x, BigInteger* m
     
     r->copy(&a);
 }
+
+/**
+ *  from http://grepcode.com/file_/repository.grepcode.com/java/root/jdk/openjdk/6-b14/java/math/BigInteger.java/?v=source
+ */
+//void BigInteger::montgomeryReduction_ColinPlumb(BigInteger* n, BigInteger* mod, int mlen, int inv) 
+//{
+//        int c=0;
+//        int len = mlen;
+//        int offset=0;
+//
+//        do 
+//        {
+//            int nEnd = n->m_data[n->m_size-1-offset];
+//            int carry = mulAdd(n, mod, offset, mlen, inv * nEnd);
+//            c += incLimb(n, offset, mlen, carry);
+//            offset++;
+//        } while(--len > 0);
+//
+//        while(c>0)
+//            c += subN(n, mod, mlen);
+//
+//        while (intArrayCmpToLen(n, mod, mlen) >= 0)
+//            subN(n, mod, mlen);
+//
+//        //return n;
+//    }
+
+
