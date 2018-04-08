8f4cf0f5cd577f5fbcd270b7ade7abd44b0c1aba
David Castells-Rufas <david.castells@uab.es>
2018-04-08 22:57:49 +0200

    initial

--- c_apint/apint_montgomeryMultMod.cpp
+++ c_apint/apint_montgomeryMultMod.cpp
@@ -14,4 +14,71 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+#include "../big_integer_apint.h"
+#include "../assertf.h"
 
+ap_uint<NUM_BIG_INTEGER_APINT_BITS> big_integer_apint_montgomeryMultBase2(ap_uint<NUM_BIG_INTEGER_APINT_BITS> x,
+        ap_uint<NUM_BIG_INTEGER_APINT_BITS> y,
+        ap_uint<NUM_BIG_INTEGER_APINT_BITS> m)
+{
+    if (big_integer_apint_extraChecks)
+    {
+        assert(x < m);
+        assert(y < m);
+        assertf(NUM_BIG_INTEGER_APINT_BITS > big_integer_apint_getLength(m),
+                "r size = %d > m limbs = %d not true!", NUM_BIG_INTEGER_APINT_BITS, big_integer_apint_getLength(m));
+    }
+    
+    ap_uint<NUM_BIG_INTEGER_APINT_BITS> r = 0;
+    
+    //int n = m->getLimbLength();
+    int n = big_integer_apint_getLength(m);
+    
+    // initialize variables used in the loop
+//    unsigned int u[NUM_BIG_INTEGER_ARRAY_LIMBS];
+    
+    ap_uint<NUM_BIG_INTEGER_APINT_BITS> t2;    
+    ap_uint<NUM_BIG_INTEGER_APINT_BITS> t3;
+    
+    // i are bits now!
+    for (int i=0; i < n; i++)
+    {
+        // ui = (a0 + xi*y0 ) * m' mod b
+        // since m' = 1 and b is 2...
+        // ui = (a0 + xi*y0 ) % 2
+        
+//        unsigned int ui = (big_integer_array_getBit(r, 0) + 
+//            big_integer_array_getBit(x, i) * big_integer_array_getBit(y, 0)) % 2;
+        
+        // A = (A  + xi * y + ui * m) / b
+        
+        // second term
+        if (big_integer_apint_getBit(x, i))
+            t2 = y;
+        else
+            t2 = 0;
+        
+        r = r + t2;
+//        cout << " r = " << r->toHexString() << endl;
+        
+        // third term
+        if (big_integer_apint_isOdd(r))
+            t3 = m;
+        else
+            t3 = 0;
+        
+        r = r + t3;
+//        cout << " r = " << r->toHexString() << endl;
+
+        r = r >> 1;
+
+    }
+    
+    if (r >= m)
+    {
+        r = r - m;
+//        cout << " r = " << r->toHexString() << endl;
+    }
+    
+    return r;
+}
