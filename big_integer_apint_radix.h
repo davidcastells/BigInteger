35c0ec24a2056baccd19df2fb55cddf6574db25e
David Castells-Rufas <david.castells@uab.es>
2018-05-19 17:12:04 +0200

    mult

--- big_integer_apint_radix.h
+++ big_integer_apint_radix.h
@@ -31,14 +31,18 @@
 #define NUM_BIG_INTEGER_APINT_RADIX_BITS    (2048+NUM_BIG_INTEGER_APINT_RADIX)
 #define NUM_BIG_INTEGER_APINT_RADIX_LIMBS   (NUM_BIG_INTEGER_APINT_RADIX_BITS/NUM_BIG_INTEGER_APINT_RADIX)
 
+extern int big_integer_apint_radix_extraChecks;
 
 typedef ap_uint<NUM_BIG_INTEGER_APINT_RADIX> limbs_radix_array[NUM_BIG_INTEGER_APINT_RADIX_LIMBS];   // big integer array
 typedef ap_uint<NUM_BIG_INTEGER_APINT_RADIX> limbs_radix_array2[NUM_BIG_INTEGER_APINT_RADIX_LIMBS*2];  // big integer array (double size)
 
 int big_integer_apint_radix_isEqual(limbs_radix_array a, limbs_radix_array b);
+int big_integer_apint_radix_getLength(limbs_radix_array m);
 int big_integer_apint_radix_getLimbLength(limbs_radix_array m);
+int big_integer_apint_radix_isNegative(limbs_radix_array v);
 
 void big_integer_apint_radix_add(limbs_radix_array r, limbs_radix_array a, limbs_radix_array b);
+void big_integer_apint_radix_mult(limbs_radix_array r, limbs_radix_array a, limbs_radix_array b);
 void big_integer_apint_radix_zero(limbs_radix_array r);
 void big_integer_apint_radix_initfromLimbArray(limbs_radix_array r, unsigned int* m_data, unsigned int limbs);
 
