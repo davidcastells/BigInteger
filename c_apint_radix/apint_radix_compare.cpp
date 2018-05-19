b8aca963132989197be68274dcc685239487d294
David Castells-Rufas <david.castells@uab.es>
2018-05-19 16:59:26 +0200

    isNegative

--- c_apint_radix/apint_radix_compare.cpp
+++ c_apint_radix/apint_radix_compare.cpp
@@ -26,3 +26,10 @@ int big_integer_apint_radix_isEqual(limbs_radix_array a, limbs_radix_array b)
 
     return true;
 }
+
+int big_integer_apint_radix_isNegative(limbs_radix_array v)
+{
+    ap_uint<NUM_BIG_INTEGER_APINT_RADIX> one = 1;
+    ap_uint<NUM_BIG_INTEGER_APINT_RADIX> negBit = (one << (NUM_BIG_INTEGER_APINT_RADIX-1));
+    return (v[NUM_BIG_INTEGER_APINT_RADIX_LIMBS-1] & negBit)? 1 : 0;
+}
