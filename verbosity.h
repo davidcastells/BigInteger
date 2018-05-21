42ccda855016cbf16c399879ff3692b4847e2f54
David Castells-Rufas <david.castells@uab.es>
2018-05-21 19:55:58 +0200

    initial

---
+++ verbosity.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2017 Universitat Autonoma de Barcelona - David Castells-Rufas <david.castells@uab.cat>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* 
+ * File:   verbosity.h
+ * Author: dcr
+ *
+ * Created on May 20, 2018, 9:46 AM
+ */
+
+#ifndef VERBOSITY_H
+#define VERBOSITY_H
+
+#define VERBOSITY_LEVEL_ADD             6
+#define VERBOSITY_LEVEL_DIV             5
+#define VERBOSITY_LEVEL_GET_LENGTH      6
+#define VERBOSITY_LEVEL_MULT            5
+#define VERBOSITY_LEVEL_MULT_KARATSUBA  5
+#define VERBOSITY_LEVEL_MULT_MONTGOMERY 4
+#define VERBOSITY_LEVEL_MULT_MOD        5
+#define VERBOSITY_LEVEL_MONTGOMERY      3
+#define VERBOSITY_LEVEL_POWER_MOD       2
+#define VERBOSITY_LEVEL_INV_MOD         4
+#define VERBOSITY_LEVEL_RANGE           6
+
+
+#endif /* VERBOSITY_H */
+
